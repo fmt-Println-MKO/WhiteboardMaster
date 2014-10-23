058ef0fdb13d8228cc7728b10a1da41571caeb61
Matthias Koch <dev@opacum.com>
2014-10-23 20:41:25 +0200

    added LRU Cache for whiteboard list UI

--- app/src/main/java/co/whiteboardmaster/android/adapter/WhiteboardCursorAdapter.java
+++ app/src/main/java/co/whiteboardmaster/android/adapter/WhiteboardCursorAdapter.java
@@ -2,10 +2,7 @@ package co.whiteboardmaster.android.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.util.Log;
-import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +13,6 @@ import co.whiteboardmaster.android.R;
 import co.whiteboardmaster.android.model.ThumbImageMessage;
 import co.whiteboardmaster.android.model.Whiteboard;
 import co.whiteboardmaster.android.utils.ImageLoaderThread;
-import co.whiteboardmaster.android.utils.PictureUtils;
 import co.whiteboardmaster.android.utils.WhiteboardDatabaseHelper;
 
 /**
@@ -31,30 +27,10 @@ public class WhiteboardCursorAdapter extends CursorAdapter {
     private ImageLoaderThread mImageLoaderThread;
 
 
-    private LruCache<String, Bitmap> mMemoryCache;
-
     public WhiteboardCursorAdapter(Context context, WhiteboardDatabaseHelper.WhiteboardCursor cursor, ImageLoaderThread imageLoaderThread) {
         super(context, cursor, 0);
         mCursor = cursor;
         mImageLoaderThread = imageLoaderThread;
-
-        // Get max available VM memory, exceeding this amount will throw an
-        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
-        // int in its constructor.
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-
-        // Use 1/8th of the available memory for this memory cache.
-        final int cacheSize = maxMemory / 8;
-
-        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                // The cache size will be measured in kilobytes rather than
-                // number of items.
-                return bitmap.getByteCount() / 1024;
-            }
-        };
-
     }
 
 
@@ -80,13 +56,30 @@ public class WhiteboardCursorAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         Whiteboard wb = mCursor.getWhiteboard();
 //        Log.d(TAG," -------------  courser pos:" + mCursor.getPosition());
-//        Log.d(TAG," -------------  whiteboard: " + wb);
+//        Log.d(TAG, " -------------  whiteboard: " + wb.getId());
         ThumbImageMessage imageMessage = (ThumbImageMessage) view.getTag();
         imageMessage.path = wb.getPath();
-        imageMessage.imageId= wb.getId();
+        imageMessage.imageId = wb.getId();
         imageMessage.mProgressContainer.setVisibility(View.VISIBLE);
-        PictureUtils.cleanImageView(imageMessage.imageView);
-        mImageLoaderThread.queueImage(imageMessage,wb.getId());
+        imageMessage.imageView.setImageDrawable(null);
+
+        BitmapDrawable cachedImage = mImageLoaderThread.getBitmapFromMemCache(wb.getId());
+
+//        Log.d(TAG, " -------------  cachedImage: " + cachedImage);
+//        if (cachedImage != null) {
+//            Log.d(TAG, " ------------- recycled: " + cachedImage.getBitmap().isRecycled() + " -- " + cachedImage.getBitmap());
+//        }
+//
+        if (cachedImage == null || cachedImage.getBitmap().isRecycled()) {
+//            Log.d(TAG, " ------------- null or recycled: " );
+            mImageLoaderThread.queueImage(imageMessage, wb.getId());
+        } else {
+//            Log.d(TAG, " ------------- using: " + cachedImage.getBitmap().isRecycled() + " hash: " + cachedImage.getBitmap());
+            imageMessage.mProgressContainer.setVisibility(View.INVISIBLE);
+            imageMessage.imageView.setImageDrawable(cachedImage);
+
+//            mListener.onImageLoaded(token, image);
+        }
 //        System.out.println("--------size: " + wb.getId() +" -  " + wb.getGuid());
     }
 
@@ -95,15 +88,4 @@ public class WhiteboardCursorAdapter extends CursorAdapter {
 //        Log.d(TAG," -------------  item pos:" + position);
         return mCursor.getWhiteboard(position);
     }
-
-    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-        if (getBitmapFromMemCache(key) == null) {
-            mMemoryCache.put(key, bitmap);
-        }
-    }
-
-    public Bitmap getBitmapFromMemCache(String key) {
-        return mMemoryCache.get(key);
-    }
-
 }
